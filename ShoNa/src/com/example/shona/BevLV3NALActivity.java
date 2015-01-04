f68dba096da20fa3b443b52ba57a4e25e0df2a7b
kvlch <kvlchankrachang@gmail.com>
2015-01-04 20:10:44 +0700

    rename classes

--- ShoNa/src/com/example/shona/BevLV3NALActivity.java
+++ ShoNa/src/com/example/shona/BevLV3NALActivity.java
@@ -8,7 +8,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class LV3ALActivity extends Activity {
+public class BevLV3NALActivity extends Activity {
 	/*
 	 * view components
 	 */
@@ -24,35 +24,40 @@ public class LV3ALActivity extends Activity {
 	private Button LV3CateBut7;
 	private Button LV3CateBut8;
 	private Button LV3CateBut9;
+	private Button LV3CateBut10;
 	
 	//OnCLickListener
 	private OnClickListener LV3CLS;
+	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_lv3_al);
+		setContentView(R.layout.activity_lv3_nal);
 		//textview
 		LV3des = (TextView) findViewById(R.id.textView1);
 		LV3des.setContentDescription("Please choose a product category");
 		//buttons
 		LV3CateBut1 = (Button)findViewById(R.id.button1);
-		LV3CateBut1.setContentDescription("Spirit");
+		LV3CateBut1.setContentDescription("Milk");
 		LV3CateBut2 = (Button)findViewById(R.id.button2);
-		LV3CateBut2.setContentDescription("White Wine");
+		LV3CateBut2.setContentDescription("Health & Beauty Drinks/Supplements");
 		LV3CateBut3 = (Button)findViewById(R.id.button3);
-		LV3CateBut3.setContentDescription("Red Wine");
+		LV3CateBut3.setContentDescription("Energy & Sport Drinks");
 		LV3CateBut4 = (Button)findViewById(R.id.button4);
-		LV3CateBut4.setContentDescription("Rosae Wine");
+		LV3CateBut4.setContentDescription("Soft Drinks");
 		LV3CateBut5 = (Button)findViewById(R.id.button5);
-		LV3CateBut5.setContentDescription("Champagne & Sparkling Wine");
+		LV3CateBut5.setContentDescription("Fruit Juice");
 		LV3CateBut6 = (Button)findViewById(R.id.button6);
-		LV3CateBut6.setContentDescription("Asian Rice Wine");
+		LV3CateBut6.setContentDescription("Water");
 		LV3CateBut7 = (Button)findViewById(R.id.button7);
-		LV3CateBut7.setContentDescription("Coolers Mixed & Mixer");
+		LV3CateBut7.setContentDescription("Coffee");
 		LV3CateBut8 = (Button)findViewById(R.id.button8);
-		LV3CateBut8.setContentDescription("Beer");
+		LV3CateBut8.setContentDescription("Coffee Creamers");
 		LV3CateBut9 = (Button)findViewById(R.id.button9);
-		LV3CateBut9.setContentDescription("Accessories");
+		LV3CateBut9.setContentDescription("Tea");
+		LV3CateBut10 = (Button)findViewById(R.id.button10);
+		LV3CateBut10.setContentDescription("Powder Health Tonics");
 		
 		//set OnClickListener
 		LV3CateBut1.setOnClickListener(LV3CLS);
@@ -64,12 +69,13 @@ public class LV3ALActivity extends Activity {
 		LV3CateBut7.setOnClickListener(LV3CLS);
 		LV3CateBut8.setOnClickListener(LV3CLS);
 		LV3CateBut9.setOnClickListener(LV3CLS);
+		LV3CateBut10.setOnClickListener(LV3CLS);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.lv3_al, menu);
+		getMenuInflater().inflate(R.menu.lv3_nal, menu);
 		return true;
 	}
 
