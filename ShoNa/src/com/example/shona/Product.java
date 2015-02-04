434c7f4563b61fc4ab0715c9c363e164dbf10a26
pun2345 <pun_taweekiat@hotmail.com>
2015-02-04 11:07:50 +0700

    Add class Product

---
+++ ShoNa/src/com/example/shona/Product.java
@@ -0,0 +1,17 @@
+package com.example.shona;
+
+public class Product {
+	protected int id;
+	protected String name;
+	protected double price;
+	protected String description;
+	
+	public Product(int id, String name, Double price, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.description = description;
+	}
+	
+}
